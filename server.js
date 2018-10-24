7cfb1791a6192ab6aa9292bac7c7addca2b20ce7
JennAh <jennah@Jenns-Air.home>
2018-10-23 19:35:37 -1000

    created header readFile function response

--- server.js
+++ server.js
@@ -6,106 +6,46 @@ const server = http.createServer((request, response) => {
   const uri = request.url;
   const reqMethod = request.method;
   const reqVersion = request.httpVersion;
-  //console.log('this is my uri', uri);
-  console.log('this is my method', reqMethod);
-  console.log('this is my uri', uri);
-  console.log('this is my httpVersion', reqVersion);
 
-console.log('request body', request.body);
+  function readF(fileLocation, conType, statusCode, reasonPhrase) {
+    fs.readFile(fileLocation, (err, data) => {
+      if (err) throw err;
+      response.setHeader(`Content-Type`, conType);
+      response.setHeader(`Content-Length`, data.length);
+      response.writeHead(statusCode, reasonPhrase);
+      response.write(data);
+      response.end();
+    });
+  };
 
   switch (uri) {
     case '/':
     case '/index.html':
-      //console.log(createHeader('text/html', element.length, 200, 'OK', data));
-      //response.write('./index.html');
-      fs.readFile('./public/index.html', 'utf8', (err, data) => {
-        if (err) throw err;
 
-        response.setHeader('Content-Type', 'text/html');
-        response.setHeader('Content-Length', data.length);
-        response.writeHead(200, 'OK');
-        response.write(data);
-        response.end();
-      });
-
-
+      readF('./public/index.html', 'text/html', 200, 'OK');
       break;
 
     case '/hydrogen.html':
-      //hydrogen response
-      fs.readFile('./public/hydrogen.html', 'utf8', (err, data) => {
-        if (err) throw err;
 
-        response.setHeader('Content-Type', 'text/html');
-        response.setHeader('Content-Length', data.length);
-        response.writeHead(200, 'OK');
-        response.write(data);
-        response.end();
-
-      });
+      readF('./public/hydrogen.html', 'text/html', 200, 'OK');
       break;
 
     case '/helium.html':
 
-      fs.readFile('./public/helium.html', 'utf8', (err, data) => {
-        if (err) throw err;
-
-        response.setHeader('Content-Type', 'text/html');
-        response.setHeader('Content-Length', data.length);
-        response.writeHead(200, 'OK');
-        response.write(data);
-        response.end();
-      });
+      readF('./public/helium.html', 'text/html', 200, 'OK');
       break;
-
 
     case '/css/styles.css':
 
-      fs.readFile('./public/css/styles.css', 'utf8', (err, data) => {
-        if (err) throw err;
-
-        response.setHeader('Content-Type', 'text/css');
-        response.setHeader('Content-Length', data.length);
-        response.writeHead(200, 'OK');
-        response.write(data);
-        response.end();
-      });
+      readF('./public/css/styles.css', 'text/css', 200, 'OK');
       break;
 
-      case '/elements':
-      //reqMethod = 'POST';
-      response.setHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-
-      response.end();
     default:
 
-      fs.readFile('./public/404.html', 'utf8', (err, data) => {
-        if (err) throw err;
-
-        response.setHeader('Content-Type', 'text/html');
-        response.setHeader('Content-Length', data.length);
-        response.writeHead(404, 'Not Found');
-        response.write(data);
-        response.end();
-      });
-
+      readF('./public/404.html', 'text/html', 404, 'Not Found');
   }
 });
 
 server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
-
-
-
-
-// function createHeader (contentType, contentLength, statusCode, reasonPhrase, writeThis) {
-
-//   response.setHeader(`Content-Type ${contentType}`);
-//   response.setHeader(`Content-Length ${contentLength}`);
-//   response.writeHead(statusCode, reasonPhrase);
-//   response.write(writeThis);
-//   response.end();
-
-// };
