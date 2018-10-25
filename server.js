b08fabca53ea9441e641535940ca4f1d54094fd7
JennAh <jennah@Jenns-Air.home>
2018-10-24 15:20:33 -1000

    able to write elements file

--- server.js
+++ server.js
@@ -1,86 +1,75 @@
 const http = require('http');
 const fs = require('fs');
+const qs = require('querystring');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((request, response) => {
-  const uri = request.url;
-  const reqMethod = request.method;
+  //const res = response;
+  let uri = request.url;
+  let reqMethod = request.method;
   const reqVersion = request.httpVersion;
 
-  function readF(fileLocation, conType, statusCode, reasonPhrase) {
-    fs.readFile(fileLocation, (err, data) => {
-      if (err) throw err;
-      response.setHeader(`Content-Type`, conType);
-      response.setHeader(`Content-Length`, data.length);
-      response.writeHead(statusCode, reasonPhrase);
-      response.write(data);
-      response.end();
-    });
-  };
 
-  // function writeF(fileLocation, conType, statusCode, reasonPhrase) {
-  //   fs.writeFile(fileLocation, (err, data) => {
-  //     if (err) throw err;
-  //     response.setHeader(`Content-Type`, conType);
-  //     response.setHeader(`Content-Length`, data.length);
-  //     response.writeHead(statusCode, reasonPhrase);
-  //     response.write(data);
-  //     response.end();
-  //   });
-  // };
+  // response.on('data', (chunk) => {
+  //   console.log(`Body:, ${chunk}`);
+  // })
+
+  //console.log('this is my request', request);
 
   //testing nested switch cases
+  //console.log('reqMethod', reqMethod);
   switch (reqMethod) {
     case 'GET':
-      switch (uri) {
-        case '/': readF('./public/index.html', 'text/html', 200, 'OK');
-          break;
-        case '/index.html': readF('./public/index.html', 'text/html', 200, 'OK');
-          break;
-        case '/hydrogen.html': readF('./public/hydrogen.html', 'text/html', 200, 'OK');
-          break;
-        case '/helium.html': readF('./public/helium.html', 'text/html', 200, 'OK');
-          break;
-        case '/css/styles.css': readF('./public/css/styles.css', 'text/css', 200, 'OK');
-          break;
-        default: readF('./public/404.html', 'text/html', 404, 'Not Found');
-          break;
-      }
+
+      getHandler(uri, response);
+      // response.end();
       break;
 
     case 'POST':
-      switch (uri) {
-        case '/elements':
+      //switch (uri) {
+      //case '/elements':
 
-          console.log('request yo,', request);
-          // fs.writeFile('message.txt', data, (err) => {
-          //   if (err) throw err;
+      request.on('data', (chunk) => {
+        let body = qs.parse(`${chunk}`);
+        //console.log('elements', body);
+        let elemForm = `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <title>The Elements - ${body.elementName}</title>
+            <link rel="stylesheet" href="/css/styles.css">
+          </head>
+          <body>
+            <h1>${body.elementName}</h1>
+            <h2>${body.elementSymbol}</h2>
+            <h3>Atomic number ${body.elementAtomicNumber}</h3>
+            <p>${body.elementDescription}</p>
+            <p><a href="/">back</a></p>
+          </body>
+          </html>`;
 
-          //   response.setHeader(`Content-Type`, 'application/x-www-form-urlencoded');
-          //   response.setHeader(`Content-Length`, data.length);
-          //   //response.writeHead(200, reasonPhrase);
-          //   response.write(data);
-          //   response.end();
+        fs.writeFile(`./public/${body.elementName}.html`, elemForm, (err) => {
+          if (err) {
+            fs.readFile(`./public/404.html`, (err, data) => {
+              if (err) throw err;
+              response.setHeader(`Content-Type`, `text/html`);
+              response.writeHead(404, 'Not Found');
+              response.write(data);
+              response.end();
+            });
+          } else {
+            response.setHeader(`Content-Type`, `application/json`);
+            response.writeHead(200, 'OK');
+            response.write(`{ "success" : true }`);
+            response.end();
 
+          }
+        });
 
+       // response.end();
+      });
 
-
-          //   console.log('File has been saved');
-          // });
-
-
-          // fs.writeFile(fileLocation, (err, data) => {
-          //       if (err) throw err;
-          //       response.setHeader(`Content-Type`, conType);
-          //       response.setHeader(`Content-Length`, data.length);
-          //       response.writeHead(statusCode, reasonPhrase);
-          //       response.write(data);
-          //       response.end();
-          //     });
-
-
-          response.end();
-      }
+      //}
       break;
 
     case 'PUT':
@@ -95,8 +84,38 @@ const server = http.createServer((request, response) => {
       //write else code
       break;
   }
+
 });
+
+function getHandler(uri, res) {
+  //readF()
+  fs.readFile(`./public${uri}`, (err, data) => {
+    if (err) {
+      fs.readFile(`./public/404.html`, (err, data) => {
+        if (err) throw err;
+        res.setHeader(`Content-Type`, `text/html`);
+        res.writeHead(404, 'Not Found');
+        res.write(data);
+        res.end();
+      });
+    } else {
+      res.setHeader(`Content-Type`, `text/${grabType(uri)}`);
+      res.setHeader(`Content-Length`, data.length);
+      res.writeHead(200, 'OK');
+      res.write(data);
+      res.end();
+    }
+  })
+};
+
+function grabType(uri) {
+  let type = uri.split('.');
+  return type[type.length - 1];
+}
+
+
 
 server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
+
